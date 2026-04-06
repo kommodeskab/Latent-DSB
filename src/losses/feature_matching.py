ead613ad6f95c2259dde17c5067cd2ed081ed845
andreas-desktop <s214630@dtu.dk>
2026-04-06 14:30:43 +0200

    Fixed feature matching loss

--- src/losses/feature_matching.py
+++ src/losses/feature_matching.py
@@ -4,29 +4,16 @@ from transformers import Wav2Vec2Model, HubertModel, AutoModel
 from torch import Tensor
 from src.losses.baseloss import BaseLossFunction
 from src import AudioBatch, ModelOutput, LossOutput
+import torch.nn as nn
+from typing import Union
 
 
-class BaseFeatureExtractor:
-    model: Wav2Vec2Model | HubertModel
-
-    def __call__(self, audio: Tensor) -> list[Tensor]: ...
-    def to(self, device: torch.device): ...
-
-
-class Wav2VecFeatureExtractor:
+class Wav2VecFeatureExtractor(nn.Module):
     def __init__(self):
-        self.model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base-960h")
-        self.model.eval()
-
-        for param in self.model.parameters():
-            param.requires_grad = False
-
+        super().__init__()
+        self.model: Wav2Vec2Model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base-960h")
         # extract these feature layers for calculating the loss
         self.layers_to_use = [0, 4, 8, 12]
-
-    def to(self, device: torch.device):
-        self.model = self.model.to(device)
-        return self
 
     def __call__(self, audio: Tensor) -> list[Tensor]:
         # Normalize audio to have zero mean and unit variance
@@ -37,20 +24,17 @@ class Wav2VecFeatureExtractor:
         return [features[i] for i in self.layers_to_use]
 
 
-class HubertFeatureExtractor:
+class HubertFeatureExtractor(nn.Module):
     def __init__(
         self,
         last_n_conv_layers: int,
         first_n_transformer_layers: int,
     ):
+        super().__init__()
         self.last_n_conv_layers = last_n_conv_layers
         self.first_n_transformer_layers = first_n_transformer_layers
 
         self.model: HubertModel = AutoModel.from_pretrained("facebook/hubert-base-ls960")
-        self.model.eval()
-
-        for param in self.model.parameters():
-            param.requires_grad = False
 
         self.n_conv_layers = len(self.model.feature_extractor.conv_layers)
         self.n_transformer_layers = len(self.model.encoder.layers) + 1  # +1 for the feature projection layer
@@ -60,10 +44,6 @@ class HubertFeatureExtractor:
         assert (
             self.first_n_transformer_layers <= self.n_transformer_layers
         ), f"first_n_transformer_layers must be less than or equal to {self.n_transformer_layers}"
-
-    def to(self, device: torch.device):
-        self.model.to(device)
-        return self
 
     def __call__(self, audio: Tensor) -> list[Tensor]:
         feature_list = []
@@ -96,21 +76,16 @@ class HubertFeatureExtractor:
         return feature_list
 
 
+FEATURE_EXTRACTORS = Union[Wav2VecFeatureExtractor, HubertFeatureExtractor]
+
+
 class FeatureMatchingLoss(BaseLossFunction):
-    def __init__(self, feature_extractor: BaseFeatureExtractor, compile: bool = False):
+    def __init__(self, feature_extractor: FEATURE_EXTRACTORS):
         super().__init__()
         self.feature_extractor = feature_extractor
-
-        if compile:
-            self.feature_extractor.model = torch.compile(self.feature_extractor.model)
-
-    def _apply(self, fn):
-        # Called by nn.Module.to/cuda/cpu/half recursion from parent modules
-        super()._apply(fn)
-
-        probe = fn(torch.empty(0))
-        self.feature_extractor.to(probe.device)
-        return self
+        # make sure that the feature extractor is in eval mode and does not require gradients
+        self.feature_extractor.eval()
+        self.feature_extractor.requires_grad_(False)
 
     def forward(self, model_output: ModelOutput, batch: AudioBatch) -> LossOutput:
         with torch.no_grad():
@@ -123,3 +98,21 @@ class FeatureMatchingLoss(BaseLossFunction):
 
         loss = loss / len(real_features)
         return LossOutput(loss=loss)
+
+    def state_dict(self, destination=None, prefix="", keep_vars=False):
+        state_dict = super().state_dict(destination, prefix, keep_vars)
+
+        keys_to_remove = [k for k in state_dict.keys() if k.startswith(f"{prefix}feature_extractor.")]
+        for k in keys_to_remove:
+            del state_dict[k]
+
+        return state_dict
+
+    def _load_from_state_dict(
+        self, state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys, error_msgs
+    ):
+        extractor_state = self.feature_extractor.state_dict(prefix=f"{prefix}feature_extractor.")
+        state_dict.update(extractor_state)
+        super()._load_from_state_dict(
+            state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys, error_msgs
+        )
